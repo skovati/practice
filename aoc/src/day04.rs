774c79f1c092a5677b5fbf23125aff32a43a62a8
skovati <skovati@protonmail.com>
2022-01-01 14:20:09 -0600

    add day4 pt1 refactor and pt2

--- aoc/src/day04.rs
+++ aoc/src/day04.rs
@@ -1,68 +1,137 @@
 use::std::fs;
-use::std::fmt::Debug;
-use::std::fmt;
+use::std::collections::HashSet;
 
-// entry struct to store bingo info
-struct Entry {
-    num: i32,
-    called: bool,
+// typedef so we can organize the whole bingo game as a Vec<Board>
+#[derive(Clone)]
+struct Board {
+    rows: Vec<HashSet<i32>>,
+    cols: Vec<HashSet<i32>>,
 }
 
-impl Debug for Entry {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "val: {}, called: {}", self.num, self.called)
+impl Board {
+    fn parse(lines: &mut core::str::Lines) -> Option<Board> {
+        let nums = lines
+            .take(6)                        // break into chunks of 5 for each board
+            .filter(|l| !l.is_empty())      // get rid of empty lines
+            .map(|l| {
+                l.split_whitespace()        // for each line, split into nums
+                    .map(|n| 
+                         n.parse::<i32>().unwrap())
+                    .collect::<Vec<i32>>()      // parse and collect into vector
+            })
+            .collect::<Vec<Vec<i32>>>();    // and collect each set of 5 vectors into another vector (board)
+        if nums.len() == 0 { return None; }
+        let mut rows: Vec<HashSet<i32>> = Vec::new();
+        let mut cols: Vec<HashSet<i32>> = Vec::new();
+
+        // row wise
+        for row in &nums {
+            let mut set = HashSet::new();
+            for col in row {
+                set.insert(*col);
+            }
+            rows.push(set);
+        }
+
+        // col wise
+        for col in 0..nums[0].len() {
+            let mut set = HashSet::new();
+            for row in 0..nums[col].len() {
+                set.insert(nums[row][col]);
+            }
+            cols.push(set);
+        }
+
+        Some(Board {
+            rows,
+            cols,
+        })
+    }
+
+    fn draw(&mut self, n: i32) {
+        self.rows.iter_mut().for_each(|row| {
+            row.remove(&n);
+        });
+        self.cols.iter_mut().for_each(|col| {
+            col.remove(&n);
+        });
+    }
+
+    fn has_complete(&self) -> bool {
+        let mut complete = false;
+        for row in self.rows.iter() {
+            if row.is_empty() {
+                complete = true;
+            }
+        }
+        for col in self.cols.iter() {
+            if col.is_empty() {
+                complete = true;
+            }
+        }
+
+        complete
+    }
+
+    fn sum_remaining(&self) -> i32 {
+        self.rows.iter().flatten().sum()
     }
 }
 
-// typedef so we can organize the whole bingo game
-// as a Vec<Board>
-type Board = Vec<Vec<Entry>>;
-
 fn main() {
-        // get iter of lines
-        let input = fs::read_to_string("input/day04.txt")
-            .expect("File not found!");
-        let mut lines = input.lines();
-        // parse first line into vec of rand numbers to "call"
-        let drawn: Vec<i32> = lines
-            .nth(0)
-            .unwrap()
-            .split(",")
-            .map(|s| s.parse::<i32>() .unwrap())
-            .collect();
+    // get iter of lines
+    let input = fs::read_to_string("input/day04.txt")
+        .expect("File not found!");
+    let mut lines = input.lines();
+    // parse first line into vec of rand numbers to "call"
+    let drawn: Vec<i32> = lines
+        .nth(0)
+        .unwrap()
+        .split(",")
+        .map(|s| s.parse::<i32>() .unwrap())
+        .collect();
 
-        let mut game: Vec<Board> = Vec::new();
-        // parse rest into Vec<Board>
-        let boards = lines
-            .filter(|l| !l.is_empty())      // get rid of empty lines
-            .collect::<Vec<&str>>()
-            .chunks(5)                      // break into chunks of 5 for each board
-            .map(|s| s.into())
-            .collect::<Vec<Vec<&str>>>();
-        
-        for board in boards {
-            game.push(
-                board.iter().map(|row| {
-                    row.split_whitespace().map(|n| Entry {
-                        num: n.parse::<i32>().expect(n),
-                        called: false,
-                    }).collect()
-                }).collect()
-            );
-        }
+    let mut game: Vec<Board> = Vec::new();
+    // parse rest into Vec<Board>
+    while let Some(board) = Board::parse(&mut lines) {
+        game.push(board);
+    }
 
-        // loop, updating all boards on each draw
-        for num in drawn {
-            for board in game {
-                for row in board {
-                    for mut entry in row {
-                        if entry.num == num {
-                            entry.called = true;
-                        }
-                    }
+    // part 1
+    {
+        // loop over all drawn numbers
+        let mut game_pt1 = game.clone();
+        'pt1: for n in drawn.iter() {
+            for board in game_pt1.iter_mut() {
+                board.draw(*n);
+                if board.has_complete() {
+                    println!("part 1: {}", *n * board.sum_remaining());
+                    break 'pt1;
                 }
             }
         }
-        
-        // if all of a row or col are have called == true, break and calculate score
+    }
+
+    // part 2
+    {
+        // loop over all drawn numbers
+        let mut game_pt2 = game.clone();
+        let mut result = 0;
+        for n in drawn.iter() {
+            let mut to_rem = Vec::new();
+            for (i, board) in game_pt2.iter_mut().enumerate() {
+                board.draw(*n);
+                if board.has_complete() {
+                    result = *n * board.sum_remaining();
+                    to_rem.push(i);
+                }
+            }
+
+            // after looping through all boards, remove so we update game_pt2
+            while let Some(i) = to_rem.pop() {
+                game_pt2.remove(i);
+            }
+        }
+        println!("part 2: {}", result);
+    }
 }
