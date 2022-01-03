14d1809cce8dffa433e54438cc1951b79ad72ced
skovati <skovati@protonmail.com>
2022-01-03 13:25:32 -0600

    add day5 pt1

--- aoc/src/day05.rs
+++ aoc/src/day05.rs
@@ -12,6 +12,19 @@ impl Point {
         if self.x > self.y { self.x } else { self.y } }
 }
 
+fn print_graph(graph: &Vec<Vec<i32>>) {
+    print!("[");
+    for col in 0..graph.len() {
+        print!("[");
+        for row in 0..graph[col].len() {
+            print!("{}", graph[row][col]);
+        }
+        println!("]");
+    }
+    print!("]");
+    println!();
+}
+
 fn main() {
     // parse input into starting Vec<Point> and ending Vec<Point>
     let input = fs::read_to_string("input/day05.txt")
@@ -40,7 +53,7 @@ fn main() {
         .collect::<Vec<Vec<Point>>>();
 
     // loop over input and find largest point to make coordinate matrix
-    let max_coord = lines
+    let max = lines
         .iter()
         .flatten()
         .max()
@@ -48,25 +61,37 @@ fn main() {
         .max_coord() as usize + 1;
 
     // make graph matrix to hold all lines
-    let mut graph: Vec<Vec<i32>> = (0..max_coord).map(|_| vec![0i32; max_coord]).collect();
+    let mut graph: Vec<Vec<i32>> = (0..max).map(|_| vec![0i32; max]).collect();
 
     // loop over point vectors and add to coordinate matrix
     for line in &lines {
         // if only y changes
         if line[0].x == line[1].x {
-            for i in line[0].y..line[1].y {
-                graph[(line[0].x as usize)][i as usize] += 1;
+            if line[0].y < line[1].y {
+                for i in line[0].y..line[1].y+1 {
+                    graph[(line[0].x as usize)][i as usize] += 1;
+                }
+            } else {
+                for i in line[1].y..line[0].y+1 {
+                    graph[(line[0].x as usize)][i as usize] += 1;
+                }
             }
         } 
-        // otherwise, we know x changes
-        else {
-            for i in line[0].x..line[1].x {
-                graph[(line[0].y as usize)][i as usize] += 1;
+        // otherwise, if x changes
+        else if line[0].y == line[1].y {
+            if line[0].x < line[1].x {
+                for i in line[0].x..line[1].x+1 {
+                    graph[i as usize][(line[0].y as usize)] += 1;
+                }
+            } else {
+                for i in line[1].x..line[0].x+1 {
+                    graph[i as usize][(line[0].y as usize)] += 1;
+                }
             }
         }
+        // otherwise, not a horiz or vert line
     }
 
-    println!("{:?}", graph);
 
     // loop over updated graph and see how many are > 1
     let mut counter = 0;
