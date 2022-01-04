a958183d2cd8abf81f950d5251cc1d8964ea3076
skovati <skovati@protonmail.com>
2022-01-03 21:44:07 -0600

    add day6 pt2 and tests

--- aoc/src/day06.rs
+++ aoc/src/day06.rs
@@ -1,31 +1,91 @@
 use std::fs;
 
+// max num determined by (max timer a fish can have) + (1 to represent -1 state)
+// + (1 since we loop < MAX_NUM)
+const MAX_NUM: usize = 10;
+
+#[allow(dead_code)]
+fn print_school(s: &Vec<usize>) {
+    for i in 1..s.len() {
+        print!("{}", i-1);
+    }
+    println!();
+    for i in 1..s.len() {
+        print!("{}", s[i]);
+    }
+    println!();
+}
+
+fn count_fish(init: &Vec<usize>, days: usize) -> usize {
+    // we use an array where indicies 1-9 represent the number of fish that have
+    // timers 0-8 respectfully. this is done so we have room to represent a "-1" state,
+    // which is fish that were 0 the previous day and therefore are due to reproduce the current
+    // day and then turn into a 6. we do this so we can stick to using usize and a vector, instead
+    // of a HashMap<isize>, which would cut our representable fish size in half.
+    let mut school: Vec<usize> = vec![0; MAX_NUM];
+    // seed initial state
+    (0..MAX_NUM-1).for_each(|i| {
+        school[i+1] = init
+            .iter()
+            .filter(|n| **n == i)
+            .count();
+    });
+
+    for _day in 0..days {
+        for i in 1..MAX_NUM {
+            // decrement each fish timer
+            school[i-1] = school[i];
+        }
+        // add 6's for every fish that had a timer of 0 the previous day
+        // again, school[0] represents the fish that had a timer of 0 the previous day
+        // which are the ones that need to reproduce and turn into a 6
+        school[7] += school[0];
+        // and add new 8's that the fish birthed
+        school[9] = school[0];
+    }
+
+    // the total number of fish in the school is simply the sum of every element
+    // in the vector except index 0, since that holds the number of fish that reproduced the last
+    // day, which is already counted at index 7, so we don't want to double count.
+    school.iter().skip(1).sum()
+}
+
 fn main() {
-    let input: Vec<i32> = fs::read_to_string("input/day06.txt")
+    // parse input file into vector
+    let input: Vec<usize> = fs::read_to_string("input/day06.txt")
         .expect("can't read from file!")
         .trim()
         .split(',')
-        .map(|n| n.parse::<i32>().unwrap())
+        .map(|n| n.parse::<usize>().unwrap())
         .collect();
+    
+    // part 1
+    println!("fish after 80 days: {}", count_fish(&input, 80));
 
-    let mut school = input.clone();
-    // println!("initial: {:?}", school);
-    for _day in 1..81 {
-        let mut to_add = 0;
-        school.iter_mut().for_each(|f| {
-            *f -= 1;
-            if *f < 0 {
-                to_add += 1;
-                *f = 6;
-            }
-        });
-        for _ in 0..to_add {
-            school.push(8);
-        }
-        // println!("after day {}: {:?}", day, school);
-    }
+    // part 2
+    println!("fish after 256 days: {}", count_fish(&input, 256));
+}
 
-    let total = school.len();
+#[test]
+fn small_input_80() {
+    let initial_state = &vec![3,4,3,1,2];
+    assert_eq!(count_fish(initial_state, 80), 5934);
+}
 
-    println!("total: {}", total);
+#[test]
+fn small_input_256() {
+    let initial_state = &vec![3,4,3,1,2];
+    assert_eq!(count_fish(initial_state, 256), 26984457539);
+}
+
+#[test]
+fn large_input_80() {
+    let initial_state = &vec![5,1,1,3,1,1,5,1,2,1,5,2,5,1,1,1,4,1,1,5,1,1,4,1,1,1,3,5,1,1,1,1,1,1,1,1,1,4,4,4,1,1,1,1,1,4,1,1,1,1,1,5,1,1,1,4,1,1,1,1,1,3,1,1,4,1,4,1,1,2,3,1,1,1,1,4,1,2,2,1,1,1,1,1,1,3,1,1,1,1,1,2,1,1,1,1,1,1,1,4,4,1,4,2,1,1,1,1,1,4,3,1,1,1,1,2,1,1,1,2,1,1,3,1,1,1,2,1,1,1,3,1,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,3,1,1,1,1,1,1,2,1,1,2,3,1,2,1,1,4,1,1,5,3,1,1,1,2,4,1,1,2,4,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,4,3,1,2,1,2,1,5,1,2,1,1,5,1,1,1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,1,1,1,1,1,3,1,1,5,1,1,1,1,5,1,4,1,1,1,4,1,3,4,1,4,1,1,1,1,1,1,1,1,1,3,5,1,3,1,1,1,1,4,1,5,3,1,1,1,1,1,5,1,1,1,2,2];
+    assert_eq!(count_fish(initial_state, 80), 394994);
+}
+
+#[test]
+fn large_input_256() {
+    let initial_state = &vec![5,1,1,3,1,1,5,1,2,1,5,2,5,1,1,1,4,1,1,5,1,1,4,1,1,1,3,5,1,1,1,1,1,1,1,1,1,4,4,4,1,1,1,1,1,4,1,1,1,1,1,5,1,1,1,4,1,1,1,1,1,3,1,1,4,1,4,1,1,2,3,1,1,1,1,4,1,2,2,1,1,1,1,1,1,3,1,1,1,1,1,2,1,1,1,1,1,1,1,4,4,1,4,2,1,1,1,1,1,4,3,1,1,1,1,2,1,1,1,2,1,1,3,1,1,1,2,1,1,1,3,1,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,3,1,1,1,1,1,1,2,1,1,2,3,1,2,1,1,4,1,1,5,3,1,1,1,2,4,1,1,2,4,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,4,3,1,2,1,2,1,5,1,2,1,1,5,1,1,1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,1,1,1,1,1,3,1,1,5,1,1,1,1,5,1,4,1,1,1,4,1,3,4,1,4,1,1,1,1,1,1,1,1,1,3,5,1,3,1,1,1,1,4,1,5,3,1,1,1,1,1,5,1,1,1,2,2];
+    assert_eq!(count_fish(initial_state, 256), 1765974267455);
 }
