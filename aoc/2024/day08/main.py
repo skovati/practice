2d00487c426fc3095d52281b9349db06ff557184
skovati <mail@skovati.dev>
2024-12-08 19:23:07 -0800

    add aoc day08 pt2

--- aoc/2024/day08/main.py
+++ aoc/2024/day08/main.py
@@ -20,6 +20,23 @@ def get_antinodes(grid: Grid, loc1: Loc, loc2: Loc) -> list[Loc]:
 
     return [a for a in [antinode1, antinode2] if within_grid(grid, a)]
 
+def get_antinodes_pt2(grid: Grid, loc1: Loc, loc2: Loc) -> list[Loc]:
+    dy = loc2[0] - loc1[0]
+    dx = loc2[1] - loc1[1]
+
+    antinodes: list[Loc] = []
+
+    antinode_loc = loc1
+    while within_grid(grid, antinode_loc):
+        antinodes.append(antinode_loc)
+        antinode_loc = (antinode_loc[0] - dy, antinode_loc[1] - dx)
+
+    antinode_loc = loc2
+    while within_grid(grid, antinode_loc):
+        antinodes.append(antinode_loc)
+        antinode_loc = (antinode_loc[0] - dy, antinode_loc[1] - dx)
+
+    return antinodes
 
 def solve_p1(path: str):
     contents = pathlib.Path(path).read_text()
@@ -45,11 +62,35 @@ def solve_p1(path: str):
 
     return len(antinodes)
 
+def solve_p2(path: str):
+    contents = pathlib.Path(path).read_text()
+
+    antinodes = set()
+    nodes: dict[str, list[Loc]] = dict()
+
+    grid = [list(line) for line in contents.splitlines()]
+    for i, row in enumerate(grid):
+        for j, col in enumerate(row):
+            if col != ".":
+                if col not in nodes:
+                    nodes[col] = [(i, j)]
+                else:
+                    nodes.get(col, []).append((i, j))
+
+    for id, nodes in nodes.items():
+        print(f"for id: {id}, looking at permutations for nodes: {nodes}")
+        for perm in permutations(nodes, 2):
+            print(f"perm: {perm}")
+            for a in get_antinodes_pt2(grid, perm[0], perm[1]):
+                antinodes.add(a)
+
+    return len(antinodes)
+
 def main():
     print(f"p1 example solution: {solve_p1('example.txt')}")
     print(f"p1 final solution: {solve_p1('input.txt')}")
-    # print(f"p2 example solution: {solve_p2('example.txt', parallelize=False)}")
-    # print(f"p2 final solution: {solve_p2('input.txt')}")
+    print(f"p2 example solution: {solve_p2('example.txt')}")
+    print(f"p2 final solution: {solve_p2('input.txt')}")
 
 if __name__ == "__main__":
     main()
